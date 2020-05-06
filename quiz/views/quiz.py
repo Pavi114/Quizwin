709cda4317b6c624c7c733ce4368d0ecd1b1bf7b
aananthv <aananthv@outlook.com>
2020-05-06 11:50:59 +0530

    [Feat] Select round type while creating round

--- quiz/views/quiz.py
+++ quiz/views/quiz.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from quiz.mixins.requires_login import LoginRequiredMixin
 from quiz.models import Quiz
 from quiz.classes.quiz import create_quiz, get_quiz_or_404, filter_quiz_info
+from quiz.constants import RoundType
 
 
 class CreateQuizView(LoginRequiredMixin, View):
@@ -29,7 +30,8 @@ class EditQuizView(LoginRequiredMixin, View):
         quiz_wrapper = get_quiz_or_404(request.user, quiz_id)
 
         context = {
-            'quiz': quiz_wrapper.edit_info()
+            'quiz': quiz_wrapper.edit_info(),
+            'round_types': dict(RoundType.choices)
         }
         # return context
         return render(request, 'quiz/create/quiz.html', context)
